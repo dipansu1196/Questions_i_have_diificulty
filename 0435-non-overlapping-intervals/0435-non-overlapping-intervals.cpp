8eac0f370ec6138fb382bec44a4fdfd1931c246a
dipansu1196 <166242154+dipansu1196@users.noreply.github.com>
2025-04-18 05:15:00 +0530

    Update 0435-non-overlapping-intervals.cpp

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -18,3 +18,32 @@ public:
         return ans;
     }
 };
+// 2nd Approach
+
+bool cmp(vector<int>& a, vector<int>& b) {
+    return a[1] < b[1]; // sort by end time
+}
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.empty()) return 0;
+
+        sort(intervals.begin(), intervals.end(), cmp);
+        int count = 0;
+        int n = intervals.size();
+        int firstStartTime = intervals[n - 1][0]; // start time of the last interval
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (intervals[i][1] > firstStartTime) {
+                // overlap → remove this
+                count++;
+            } else {
+                // no overlap → update
+                firstStartTime = intervals[i][0];
+            }
+        }
+
+        return count;
+    }
+};
