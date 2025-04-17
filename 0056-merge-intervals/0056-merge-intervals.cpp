a82ae8bb537de83cf60417f0c9d033d6da2cafdd
dipansu1196 <166242154+dipansu1196@users.noreply.github.com>
2025-04-18 04:44:26 +0530

    Time: 3 ms (89.81%), Space: 23.9 MB (59.29%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,7 +8,7 @@ public:
         result.push_back(intervals[0]);
         for(int i=1;i<n;i++){
             if(intervals[i][0]<=result.back()[1]){  //overlapping
-                result.back()[0]= min(result.back()[0],intervals[i][0]);
+           //     result.back()[0]= min(result.back()[0],intervals[i][0]);
                 result.back()[1]=max(result.back()[1],intervals[i][1]);
             }else{
                 result.push_back(intervals[i]);
