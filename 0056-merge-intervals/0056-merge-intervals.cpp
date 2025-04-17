b40d2a02e50d41343a8c490d34d7e0e40044ac23
dipansu1196 <166242154+dipansu1196@users.noreply.github.com>
2025-04-18 04:43:47 +0530

    Time: 5 ms (57.28%), Space: 23.8 MB (80.15%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
+        int n=intervals.size();
 
-        // Bubble sort based on the starting point of each interval
-        for (int i = 0; i < n - 1; ++i) {
-            for (int j = 0; j < n - i - 1; ++j) {
-                if (intervals[j][0] > intervals[j + 1][0]) {
-                    swap(intervals[j], intervals[j + 1]);
-                }
-            }
-        }
-
+        sort(intervals.begin(),intervals.end());
         vector<vector<int>> result;
         result.push_back(intervals[0]);
-
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] <= result.back()[1]) { // overlapping
-                result.back()[1] = max(result.back()[1], intervals[i][1]);
-            } else {
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=result.back()[1]){  //overlapping
+                result.back()[0]= min(result.back()[0],intervals[i][0]);
+                result.back()[1]=max(result.back()[1],intervals[i][1]);
+            }else{
                 result.push_back(intervals[i]);
             }
         }
-
         return result;
     }
 };
