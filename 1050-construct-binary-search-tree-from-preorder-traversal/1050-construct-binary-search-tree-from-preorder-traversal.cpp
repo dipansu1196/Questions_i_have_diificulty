db67baf8d92840ecf2fb8f053d2baa645719d4c1
DIPANSU <dipansuchoudhary873@gmail.com>
2025-04-26 15:01:30 +0530

    Time: 3 ms (18.93%), Space: 16.3 MB (69.18%) - LeetHub

--- 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/1050-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,26 +11,36 @@
  */
 class Solution {
 public:
-TreeNode* build(vector<int>& pre, int prelo, int prehi,vector<int>& in,int inlo,int inhi){
-    if(prelo> prehi) return NULL;
-   
-     TreeNode* root =new TreeNode(pre[prelo]);
-      if(prelo==prehi) return root;
-     int i= inlo;
-     while(i<=inhi){
-        if(in[i]==pre[prelo]) break;
-        i++;
-     }
-     int leftCount=i- inlo;
-     int rightCount=inhi-i;
-     root->left= build(pre, prelo+1, prelo+leftCount ,in , inlo , i-1);
-     root->right= build(pre,prelo+leftCount+1,prehi,in,i+1,inhi);
-     return root;
-}
+ TreeNode* insert(TreeNode* root, int val) {
+        if(root==NULL) return new TreeNode(val);
+         TreeNode* temp= new TreeNode(val);
+         if(root->val> val){  // go left
+              if(root->left==NULL){
+root->left=temp;
+return root;
+              }else{
+              insert(root->left,val);
+              }
+         }
+         else{
+            // go right
+            
+              if(root->right==NULL){
+root->right=temp;
+return root;
+              }
+              else{
+                 insert(root->right,val);
+              }
+         }
+         return root;
+    }
+
     TreeNode* bstFromPreorder(vector<int>& pre) {
-        vector<int> in =pre;
-        sort(in.begin(),in.end());
-         int n=pre.size();
-        return build(pre,0,n-1,in,0,n-1);
+        TreeNode* root= new TreeNode(pre[0]);
+        for(int i=1;i<pre.size();i++){
+           insert(root,pre[i]);
+        }
+        return root;
     }
 };
