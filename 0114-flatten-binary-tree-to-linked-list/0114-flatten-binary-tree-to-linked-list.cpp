059f34b495b825a35c000153333cbbdd9b8b7d15
DIPANSU <dipansuchoudhary873@gmail.com>
2025-04-28 14:26:34 +0530

    Time: 0 ms (100%), Space: 17.3 MB (95.68%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -6,6 +6,7 @@ public:
         TreeNode* curr = root;
         while (curr) {
             if (curr->left) {
+               
                 TreeNode* pred = curr->left;
                 while (pred->right) {
                     pred = pred->right;
