4dab7edc82c5b41e313524e2b8847482989f5350
DIPANSU <dipansuchoudhary873@gmail.com>
2025-09-04 16:15:51 +0530

    Time: 291 ms (5.05%), Space: 83.1 MB (5.13%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -1,45 +1,67 @@
 class Solution {
 public:
-vector<vector<int>> directions{ {1, 0}, {-1, 0}, {0, 1}, {0, -1},
-            {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+    typedef pair<int, pair<int, int>> p;
+    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1},
+                                  {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
+    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-    int m=grid.size();
-    int n=     grid[0].size();
-
-    if(m==0||n==0|| grid[0][0]!=0){
-         return -1;
-    }
-    auto isSafe= [&](int x,int y){
-        return (x>=0&& x<m&& y>=0&&y<n);
-    };
-
-    queue<pair<int,int>> que;
-    que.push({0,0});
-    int level=0;
-    while(!que.empty()){
-        int N= que.size();
-
-        while(N--){
-            auto curr= que.front();
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        // Check if start or end is blocked
+        if (m == 0 || n == 0 || grid[0][0] != 0 || grid[m-1][n-1] != 0) {
+            return -1;
+        }
+        
+        // Special case: single cell
+        if (m == 1 && n == 1) {
+            return 1;
+        }
+        
+        auto isSafe = [&](int x, int y) {
+            return (x >= 0 && x < m && y >= 0 && y < n);
+        };
+        
+        vector<vector<int>> result(m, vector<int>(n, INT_MAX));
+        
+        // Fixed: Use 'que' consistently (was mixing 'que' and 'pq')
+        priority_queue<p, vector<p>, greater<p>> que;
+        
+        // Start with distance 1 (path length includes start cell)
+        que.push({1, {0, 0}});
+        result[0][0] = 1;
+        
+        while (!que.empty()) {
+            int d = que.top().first;
+            pair<int, int> node = que.top().second;
             que.pop();
-
-            int x= curr.first;
-            int y= curr.second;
-
-            if(x==m-1&& y==n-1){
-                return level+1;
+            
+            int x = node.first;
+            int y = node.second;
+            
+            // If we reached the destination
+            if (x == m - 1 && y == n - 1) {
+                return d;
             }
-
-            for(auto dir: directions){
-                int x_=x+dir[0];
-                int y_=y+dir[1];
-                if(isSafe(x_,y_)&& grid[x_][y_]==0){
-                    que.push({x_,y_});
-                    grid[x_][y_]=1;
+            
+            // Skip if we've already found a better path to this cell
+            if (d > result[x][y]) {
+                continue;
+            }
+            
+            for (auto dir : directions) {
+                int x_ = x + dir[0];
+                int y_ = y + dir[1];
+                int dist = 1;
+                
+                // Fixed: 'result' was misspelled as 'resul'
+                if (isSafe(x_, y_) && grid[x_][y_] == 0 && d + dist < result[x_][y_]) {
+                    que.push({d + dist, {x_, y_}});
+                    result[x_][y_] = d + dist;
                 }
             }
-        } level++;
-    }
-    return -1;
+        }
+        
+        return result[m-1][n-1] == INT_MAX ? -1 : result[m-1][n-1];
     }
 };
