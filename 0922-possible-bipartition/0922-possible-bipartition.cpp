0ac8550614471a1ee324a306ed033992cf02abb1
DIPANSU <dipansuchoudhary873@gmail.com>
2025-09-10 14:28:48 +0530

    Time: 44 ms (47.76%), Space: 71.7 MB (42%) - LeetHub

---
+++ 0922-possible-bipartition/0922-possible-bipartition.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+bool checkbfs(auto & adj,int source,vector<int> & color){
+    queue<int>q;
+    q.push(source);
+    color[source]=0;
+    while(!q.empty()){
+        int u= q.front();
+        q.pop();
+        for(int & v:adj[u]){
+            if(color[v]==color[u]) return false;
+            if(color[v]==-1){
+                q.push(v);
+                color[v]=1-color[u];
+            }
+        }
+    }
+    return true;
+}
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        unordered_map<int,vector<int>> adj;
+        for(auto & e: dislikes){
+            int u=e[0];
+            int v=e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int>color(n+1,-1);
+        for(int i=0;i<=n;i++){
+            if(color[i]==-1){
+              if(  !checkbfs(adj,i,color)) return false;
+            }
+        }
+        return true;
+    }
+};
